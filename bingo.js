2be70207f1d97cf1054e4e673ba044505980f43c
Euzébio Pereira da Silva Filho <euzzbs@gmail.com>
2025-02-26 11:15:45 -0300

    Add files via upload

--- bingo.js
+++ bingo.js
@@ -2,64 +2,67 @@ var numerosSorteados = [];
 
 // Função para formatar números com zero à esquerda se necessário
 function formatarNumero(numero) {
-    return numero < 10 ? "0" + numero : numero; // Adiciona o zero à frente se o número for menor que 10
+  return numero < 10 ? "0" + numero : numero; // Adiciona o zero à frente se o número for menor que 10
 }
 
 // Função para exibir os números sorteados
 function exibirNumerosSorteados() {
-    var numerosHTML = "";
-    
-    // Cria a lista de números sorteados com a formatação necessária
-    for (var i = 0; i < numerosSorteados.length; i++) {
-        numerosHTML += "<li>" + formatarNumero(numerosSorteados[i]) + "</li>"; // Aplica a formatação aos números sorteados
-    }
+  var numerosHTML = "";
 
-    // Exibe a lista de números sorteados no elemento com o id "numerosSorteados"
-    document.getElementById("numerosSorteados").innerHTML = numerosHTML;
+  // Cria a lista de números sorteados com a formatação necessária
+  for (var i = 0; i < numerosSorteados.length; i++) {
+    numerosHTML += "<li>" + formatarNumero(numerosSorteados[i]) + "</li>"; // Aplica a formatação aos números sorteados
+  }
 
-    // Atualiza a contagem de números sorteados no elemento com o id "contagemNumeros"
-    document.getElementById("contagemNumeros").textContent = numerosSorteados.length; // Atualiza o número de sorteios
+  // Exibe a lista de números sorteados no elemento com o id "numerosSorteados"
+  document.getElementById("numerosSorteados").innerHTML = numerosHTML;
+
+  // Atualiza a contagem de números sorteados no elemento com o id "contagemNumeros"
+  document.getElementById("contagemNumeros").textContent =
+    numerosSorteados.length; // Atualiza o número de sorteios
 }
 
 // Função para sortear um número
 function sortear() {
-    // Verifica se já foram sorteados todos os 75 números
-    if (numerosSorteados.length === 75) {
-        document.getElementById("botaoSortear").disabled = true; // Desabilita o botão de sorteio
-        return; // Impede o sorteio de números após todos os 75 números terem sido sorteados
-    }
+  // Verifica se já foram sorteados todos os 75 números
+  if (numerosSorteados.length === 75) {
+    document.getElementById("botaoSortear").disabled = true; // Desabilita o botão de sorteio
+    return; // Impede o sorteio de números após todos os 75 números terem sido sorteados
+  }
 
-    var numero;
-    do {
-        numero = Math.floor(Math.random() * 75) + 1;
-    } while (numerosSorteados.includes(numero)); // Garante que o número não será sorteado mais de uma vez
+  var numero;
+  do {
+    numero = Math.floor(Math.random() * 75) + 1;
+  } while (numerosSorteados.includes(numero)); // Garante que o número não será sorteado mais de uma vez
 
-    var numeroSorteadoElemento = document.getElementById("numeroSorteado");
-    setTimeout(function () {
-        numeroSorteadoElemento.textContent = formatarNumero(numero); // Exibe o número formatado
-    }, 100); // Delay de 100ms para exibir o número sorteado
+  var numeroSorteadoElemento = document.getElementById("numeroSorteado");
+  setTimeout(function () {
+    numeroSorteadoElemento.textContent = formatarNumero(numero); // Exibe o número formatado
+  }, 100); // Delay de 100ms para exibir o número sorteado
 
-    // Atrasar a atualização da contagem de números sorteados
-    setTimeout(function () {
-        numerosSorteados.push(numero); // Adiciona o número sorteado à lista
-        exibirNumerosSorteados(); // Exibe os números sorteados
-    }, 100); // Delay de 100ms para atualizar a contagem após o número ser exibido
+  // Atrasar a atualização da contagem de números sorteados
+  setTimeout(function () {
+    numerosSorteados.push(numero); // Adiciona o número sorteado à lista
+    exibirNumerosSorteados(); // Exibe os números sorteados
+  }, 100); // Delay de 100ms para atualizar a contagem após o número ser exibido
 }
 
 // Função para reiniciar o jogo
 function reiniciar() {
-    numerosSorteados = [];
-    document.getElementById("numeroSorteado").textContent = "-";
-    document.getElementById("numerosSorteados").innerHTML = "";
-    document.getElementById("contagemNumeros").textContent = "0"; // Reseta a contagem
-    document.getElementById("botaoSortear").disabled = false; // Habilita o botão novamente
+  numerosSorteados = [];
+  document.getElementById("numeroSorteado").textContent = "-";
+  document.getElementById("numerosSorteados").innerHTML = "";
+  document.getElementById("contagemNumeros").textContent = "0"; // Reseta a contagem
+  document.getElementById("botaoSortear").disabled = false; // Habilita o botão novamente
 }
 
 // Quando a página estiver totalmente carregada, execute o código
-window.onload = function() {
-    // Adiciona o evento de clique ao botão de sorteio
-    document.getElementById("botaoSortear").addEventListener("click", sortear);
+window.onload = function () {
+  // Adiciona o evento de clique ao botão de sorteio
+  document.getElementById("botaoSortear").addEventListener("click", sortear);
 
-    // Adiciona o evento de clique ao botão de reiniciar
-    document.getElementById("botaoReiniciar").addEventListener("click", reiniciar);
+  // Adiciona o evento de clique ao botão de reiniciar
+  document
+    .getElementById("botaoReiniciar")
+    .addEventListener("click", reiniciar);
 };
--- usuario/cartela.js
+++ usuario/cartela.js
@@ -1,83 +1,97 @@
 // Perguntar ao usuário quantas cartelas ele deseja
 let quantidadeCartelas = 0;
-while (quantidadeCartelas < 1 || quantidadeCartelas > 5 || isNaN(quantidadeCartelas)) {
-    quantidadeCartelas = parseInt(prompt("Quantas cartelas você deseja gerar? (Digite um número entre 1 e 5)", "1"));
-    if (quantidadeCartelas < 1 || quantidadeCartelas > 5 || isNaN(quantidadeCartelas)) {
-        alert("Por favor, insira um número válido entre 1 e 5.");
-    }
+while (
+  quantidadeCartelas < 1 ||
+  quantidadeCartelas > 5 ||
+  isNaN(quantidadeCartelas)
+) {
+  quantidadeCartelas = parseInt(
+    prompt(
+      "Quantas cartelas você deseja gerar? (Digite um número entre 1 e 5)",
+      "1"
+    )
+  );
+  if (
+    quantidadeCartelas < 1 ||
+    quantidadeCartelas > 5 ||
+    isNaN(quantidadeCartelas)
+  ) {
+    alert("Por favor, insira um número válido entre 1 e 5.");
+  }
 }
 
 // Função para gerar números aleatórios sem repetição
 function gerarNumerosAleatorios(totalNumeros) {
-    let numeros = [];
-    while (numeros.length < totalNumeros) {
-        let numero = Math.floor(Math.random() * 75) + 1;
-        if (!numeros.includes(numero)) {
-            numeros.push(numero);
-        }
+  let numeros = [];
+  while (numeros.length < totalNumeros) {
+    let numero = Math.floor(Math.random() * 75) + 1;
+    if (!numeros.includes(numero)) {
+      numeros.push(numero);
     }
-    return numeros;
+  }
+  return numeros;
 }
 
 // Função para formatar números com zero à esquerda, se necessário
 function formatarNumero(numero) {
-    return numero < 10 ? "0" + numero : numero; // Adiciona o zero à frente se o número for menor que 10
+  return numero < 10 ? "0" + numero : numero; // Adiciona o zero à frente se o número for menor que 10
 }
 
 // Função para preencher uma cartela com números aleatórios
 function preencherCartela() {
-    let numeros = gerarNumerosAleatorios(25); // Gerar 25 números para preencher a cartela
-    let cartela = document.createElement('div');
-    cartela.classList.add('cartela');
+  let numeros = gerarNumerosAleatorios(25); // Gerar 25 números para preencher a cartela
+  let cartela = document.createElement("div");
+  cartela.classList.add("cartela");
 
-    // Preencher a cartela com os números
-    let linhaIndex = 0;
-    for (let i = 0; i < 5; i++) {
-        let linha = document.createElement('div');
-        linha.classList.add('linha');
-        for (let j = 0; j < 5; j++) {
-            let celula = document.createElement('div');
-            celula.classList.add('celula');
-            
-            // Adicionar números nas células, exceto na célula central
-            if (i !== 2 || j !== 2) {
-                celula.textContent = formatarNumero(numeros[linhaIndex++]); // Aplica a formatação ao número
-            } else {
-                celula.classList.add('central');
-                celula.innerHTML = '<img src="./icone/biblia logo.png" alt="Imagem Central" class="imagem">';
-            }
+  // Preencher a cartela com os números
+  let linhaIndex = 0;
+  for (let i = 0; i < 5; i++) {
+    let linha = document.createElement("div");
+    linha.classList.add("linha");
+    for (let j = 0; j < 5; j++) {
+      let celula = document.createElement("div");
+      celula.classList.add("celula");
 
-            // Adicionar o evento de clique para marcar/desmarcar a célula
-            celula.addEventListener('click', function() {
-                // Verificar se a célula clicada é a central
-                if (!celula.classList.contains('central')) {
-                    marcarCelula(celula);
-                }
-            });
+      // Adicionar números nas células, exceto na célula central
+      if (i !== 2 || j !== 2) {
+        celula.textContent = formatarNumero(numeros[linhaIndex++]); // Aplica a formatação ao número
+      } else {
+        celula.classList.add("central");
+        celula.innerHTML =
+          '<img src="./icone/biblia logo.png" alt="Imagem Central" class="imagem">';
+      }
 
-            linha.appendChild(celula);
+      // Adicionar o evento de clique para marcar/desmarcar a célula
+      celula.addEventListener("click", function () {
+        // Verificar se a célula clicada é a central
+        if (!celula.classList.contains("central")) {
+          marcarCelula(celula);
         }
-        cartela.appendChild(linha);
-    }
+      });
 
-    // Adicionar a cartela ao corpo da página
-    document.body.appendChild(cartela);
+      linha.appendChild(celula);
+    }
+    cartela.appendChild(linha);
+  }
+
+  // Adicionar a cartela ao corpo da página
+  document.body.appendChild(cartela);
 }
 
 // Função para marcar/desmarcar uma célula
 function marcarCelula(celula) {
-    // Se a célula já estiver marcada, removemos a marcação
-    if (celula.classList.contains('marcada')) {
-        celula.classList.remove('marcada');
-        celula.style.backgroundColor = ''; // Remover a cor vermelha
-    } else {
-        // Caso contrário, marcamos a célula
-        celula.classList.add('marcada');
-        celula.style.backgroundColor = '#BF3B3B'; // Colocar a cor vermelha
-    }
+  // Se a célula já estiver marcada, removemos a marcação
+  if (celula.classList.contains("marcada")) {
+    celula.classList.remove("marcada");
+    celula.style.backgroundColor = ""; // Remover a cor vermelha
+  } else {
+    // Caso contrário, marcamos a célula
+    celula.classList.add("marcada");
+    celula.style.backgroundColor = "#BF3B3B"; // Colocar a cor vermelha
+  }
 }
 
 // Gerar o número de cartelas solicitadas pelo usuário
 for (let i = 0; i < quantidadeCartelas; i++) {
-    preencherCartela();
+  preencherCartela();
 }
